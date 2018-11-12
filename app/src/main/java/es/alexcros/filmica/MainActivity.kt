715dbb1a83ca0f4545c636ad2328302b50a9c26f
Alex Cros <alexcroscuadros@gmail.com>
2018-11-13 00:29:01 +0100

    button with toast

--- app/src/main/java/es/alexcros/filmica/MainActivity.kt
+++ app/src/main/java/es/alexcros/filmica/MainActivity.kt
@@ -2,11 +2,19 @@ package es.alexcros.filmica
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.button_add)
+
+        button.setOnClickListener { view ->
+            Toast.makeText(this@MainActivity, "Added to list", Toast.LENGTH_LONG).show()
+        }
     }
 }
