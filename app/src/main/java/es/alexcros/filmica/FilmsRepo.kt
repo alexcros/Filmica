1fc7cef096d3148c6ce6e6bb500bc5d579c9100c
Alex Cros <alexcroscuadros@gmail.com>
2018-11-13 23:06:37 +0100

    refactor filmsRepo, add label

--- app/src/main/java/es/alexcros/filmica/FilmsRepo.kt
+++ app/src/main/java/es/alexcros/filmica/FilmsRepo.kt
@@ -11,7 +11,7 @@ package es.alexcros.filmica
 object FilmsRepo {
     val films: MutableList<Film> = mutableListOf()
         get() {
-            // films instance field (avoid getter recall)
+            // films instance "field" (avoid getter recall)
             if (field.isEmpty()) {
                 field.addAll(dummyFilms())
             }
@@ -19,23 +19,16 @@ object FilmsRepo {
             return field
         }
 
-    private fun dummyFilms() : MutableList<Film> {
-        val films: MutableList<Film> = mutableListOf()
-
-        for (i in 1..9) {
-            films.add(
-                    Film(
-                            title = "Film ${i}",
-                            genre = "Genre ${i}",
-                            release =        "Release ${i}",
-                            voteRating = i.toDouble(),
-                            overview = "Overview ${i}"
-                    )
+    private fun dummyFilms(): List<Film> {
+        return (0..9).map {
+            Film(
+                    title = "Film ${it}",
+                    genre = "Genre ${it}",
+                    release = "200$it-0$it-0${it}",
+                    voteRating = it.toDouble(),
+                    overview = "Overview $it"
             )
-
         }
-
-        return films
     }
 }
 
