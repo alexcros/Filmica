f5e78febbb916129b2813b19ca41ce48cb978917
Alex Cros <alexcroscuadros@gmail.com>
2018-11-13 22:08:30 +0100

    Add Film random id

--- app/src/main/java/es/alexcros/filmica/Models.kt
+++ app/src/main/java/es/alexcros/filmica/Models.kt
@@ -1,13 +1,17 @@
 package es.alexcros.filmica
 
+import java.util.*
+
 /**
  * Created by alexandre on 13/11/18.
  */
 
-data class Film(var title: String,
-                var genre: String,
-                var release: String,
-                var voteRating: Double,
-                var overview: String
+data class Film(
+        var id: String = UUID.randomUUID().toString(),
+        var title: String,
+        var genre: String,
+        var release: String,
+        var voteRating: Double,
+        var overview: String
 )
 
