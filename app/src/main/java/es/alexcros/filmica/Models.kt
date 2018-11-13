b5609c05baeb493be40dde67f8a8f4b4d3019b49
Alex Cros <alexcroscuadros@gmail.com>
2018-11-13 21:59:22 +0100

    Add Film data class

---
+++ app/src/main/java/es/alexcros/filmica/Models.kt
@@ -0,0 +1,13 @@
+package es.alexcros.filmica
+
+/**
+ * Created by alexandre on 13/11/18.
+ */
+
+data class Film(var title: String,
+                var genre: String,
+                var release: String,
+                var voteRating: Double,
+                var overview: String
+)
+
