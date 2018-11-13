919865226b75473c49a1a15872e5b7c09a115a96
Alex Cros <alexcroscuadros@gmail.com>
2018-11-14 00:53:23 +0100

    Add Films activity and adapter

--- app/src/main/java/es/alexcros/filmica/FilmsActivity.kt
+++ app/src/main/java/es/alexcros/filmica/FilmsActivity.kt
@@ -3,6 +3,8 @@ package es.alexcros.filmica
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 
 /**
@@ -14,6 +16,9 @@ class FilmsActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_films)
+
+        val list: RecyclerView = findViewById(R.id.list_films)
+        list.layoutManager = LinearLayoutManager(this)
     }
 
     fun showDetails(clickedView: View) {
---
+++ app/src/main/java/es/alexcros/filmica/FilmsAdapter.kt
@@ -0,0 +1,7 @@
+package es.alexcros.filmica
+
+/**
+ * Created by alexandre on 13/11/18.
+ */
+class FilmsAdapter {
+}
