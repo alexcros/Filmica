65da195db1203cf4175177ed09d5aac569c6300b
Alex Cros <alexcroscuadros@gmail.com>
2018-11-19 21:43:10 +0100

    item output in adapter

--- app/src/main/java/es/alexcros/filmica/ItemOffsetDecoration.kt
+++ app/src/main/java/es/alexcros/filmica/ItemOffsetDecoration.kt
@@ -1,6 +1,7 @@
 package es.alexcros.filmica
 
 import android.graphics.Rect
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 
@@ -9,12 +10,34 @@ import android.view.View
  */
 class ItemOffsetDecoration: RecyclerView.ItemDecoration() {
 
-    override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         super.getItemOffsets(outRect, view, parent, state)
 
         // obtain space dimension pixel size
-        val offset: Int = view.context.resources.getDimensionPixelSize(R.dimen.offset_grid)
+        val offset = view.context.resources.getDimensionPixelSize(R.dimen.offset_grid)
 
-        
+        // get pixel position
+        val position = parent.getChildAdapterPosition(view)
+
+        // item count
+        val items: Int = parent.adapter?.itemCount ?: 0
+
+        // column & rows number
+        val columns = (parent.layoutManager as GridLayoutManager).spanCount
+        val rows = (items / columns)
+
+        // find item column & row
+        val column = (position % columns) + 1
+        val row = Math.ceil((position.toDouble() + 1) / columns.toDouble()).toInt()
+
+        // item spacing
+        val topOffSet = if (row == 1) offset else offset / 2
+        val leftOffSet = if (column == 1) offset else offset / 2
+        val bottomOffSet = if (row == rows) offset else offset / 2
+        val rightOffSet = if (column == columns) offset else offset / 2
+
+        // rect output
+        outRect.set(leftOffSet, topOffSet, rightOffSet, bottomOffSet)
+
     }
 }
