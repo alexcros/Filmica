da9673f20ca3a0d9f97555ce32c8543474c0fee3
Alex Cros <alexcroscuadros@gmail.com>
2018-11-19 01:46:05 +0100

    Implement onCreate view in fragment

--- app/src/main/java/es/alexcros/filmica/FilmsFragment.kt
+++ app/src/main/java/es/alexcros/filmica/FilmsFragment.kt
@@ -1,11 +1,21 @@
 package es.alexcros.filmica
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by alexandre on 19/11/18.
  */
 class FilmsFragment: Fragment() {
 
-    
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_films, container, false)
+
+    }
+
+
 }
