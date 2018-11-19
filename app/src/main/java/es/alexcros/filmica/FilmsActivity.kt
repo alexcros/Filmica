90a0360a0bc0d07cf6290323c813b35dc8edcb7f
Alex Cros <alexcroscuadros@gmail.com>
2018-11-19 01:51:14 +0100

    Refactor activity to fragment view

--- app/src/main/java/es/alexcros/filmica/FilmsActivity.kt
+++ app/src/main/java/es/alexcros/filmica/FilmsActivity.kt
@@ -11,22 +11,6 @@ import android.support.v7.widget.RecyclerView
  */
 class FilmsActivity: AppCompatActivity() {
 
-    val list: RecyclerView by lazy {
-        val instance = findViewById<RecyclerView>(R.id.list_films)
-        instance.layoutManager = LinearLayoutManager(this)
-
-        instance
-    }
-
-    val adapter: FilmsAdapter by lazy {
-        val instance = FilmsAdapter { Film ->
-            this.showDetails(Film.id)
-        }
-
-        instance.setFilms(FilmsRepo.films)
-        instance
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_films)
@@ -35,9 +19,4 @@ class FilmsActivity: AppCompatActivity() {
 
     }
 
-    fun showDetails(filmId: String) {
-        val intentToDetails = Intent(this, DetailsActivity::class.java)
-        intentToDetails.putExtra("id", filmId)
-        startActivity(intentToDetails)
-    }
 }
--- app/src/main/java/es/alexcros/filmica/FilmsFragment.kt
+++ app/src/main/java/es/alexcros/filmica/FilmsFragment.kt
@@ -1,7 +1,10 @@
 package es.alexcros.filmica
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,10 +14,33 @@ import android.view.ViewGroup
  */
 class FilmsFragment: Fragment() {
 
+    val list: RecyclerView by lazy {
+        val instance = view!!.findViewById<RecyclerView>(R.id.list_films)
+        instance.layoutManager = LinearLayoutManager(this.context)
+
+        instance
+    }
+
+    val adapter: FilmsAdapter by lazy {
+        val instance = FilmsAdapter { Film ->
+            this.showDetails(Film.id)
+        }
+
+        instance.setFilms(FilmsRepo.films)
+        instance
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_films, container, false)
 
+    }
+
+
+    fun showDetails(filmId: String) {
+        val intentToDetails = Intent(this.context, DetailsActivity::class.java)
+        intentToDetails.putExtra("id", filmId)
+        startActivity(intentToDetails)
     }
 
 
