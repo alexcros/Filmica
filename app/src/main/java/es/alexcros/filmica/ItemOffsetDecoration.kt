6736a00488fda7c39e1f82fa65b4be3afa81cff0
Alex Cros <alexcroscuadros@gmail.com>
2018-11-19 21:15:49 +0100

    ItemOffsetSetDecoration class

---
+++ app/src/main/java/es/alexcros/filmica/ItemOffsetDecoration.kt
@@ -0,0 +1,15 @@
+package es.alexcros.filmica
+
+import android.graphics.Rect
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Created by alexandre on 19/11/18.
+ */
+class ItemOffsetDecoration: RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
+        super.getItemOffsets(outRect, view, parent, state)
+    }
+}
