2b9dc1389bb602cc4dbde05ffbbbc81ce8a7148a
Alex Cros <alexcroscuadros@gmail.com>
2018-11-19 02:35:10 +0100

    fix automatic turn redraw

--- app/src/main/java/es/alexcros/filmica/FilmsActivity.kt
+++ app/src/main/java/es/alexcros/filmica/FilmsActivity.kt
@@ -3,9 +3,6 @@ package es.alexcros.filmica
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-
 /**
  * Created by alexandre on 13/11/18.
  */
@@ -15,12 +12,19 @@ class FilmsActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_films)
 
-        val filmsFragment = FilmsFragment()
+        if (savedInstanceState == null) {
+            val filmsFragment = FilmsFragment()
 
-        supportFragmentManager.beginTransaction()
-                .add(R.id.container_list, filmsFragment)
-                .commit()
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.container_list, filmsFragment)
+                    .commit()
+
+        }
 
     }
+
+
+
+
 
 }
