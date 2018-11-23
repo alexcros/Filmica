7b9f5c7f1517889c7628c5771dcdf248fb066300
Alex Cros <alexcroscuadros@gmail.com>
2018-11-23 20:06:06 +0100

    Add animation fadeImageView

--- app/src/main/java/es/alexcros/filmica/view/utils/FadeImageView.kt
+++ app/src/main/java/es/alexcros/filmica/view/utils/FadeImageView.kt
@@ -1,7 +1,30 @@
 package es.alexcros.filmica.view.utils
 
+import android.animation.Animator
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.AttributeSet
+import android.widget.ImageView
+
 /**
  * Created by alexandre on 23/11/18.
  */
-class FadeImageView {
+class FadeImageView : ImageView {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun setImageBitmap(bm: Bitmap?) {
+
+        animate()
+                .alpha(0F)
+                .setDuration(150)
+                .setListener(AnimatorEndListener {
+                    super@FadeImageView.setImageBitmap(bm)
+                    animate()
+                            .alpha(1F)
+                            .setDuration(120)
+                })
+    }
 }
